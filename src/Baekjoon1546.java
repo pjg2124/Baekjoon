7c03fd0bc5efeac07b46ec38a208bf4c29fedcfd
parkjingam <pjg2124@naver.com>
2023-07-16 17:10:42 +0900

    baekjoon1546

--- src/Baekjoon1546.java
+++ src/Baekjoon1546.java
@@ -4,19 +4,15 @@ public class Baekjoon1546 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] arr = new int[N];
         double max = 0;
         double result = 0;
 
-        for (int i =0; i< N; i++) {
-            arr[i] = sc.nextInt();
-            if (arr[i] > max) max = arr[i];
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            result += x;
+            if (x > max) max = x;
         }
 
-        for (int i =0; i< N; i++) {
-            result += arr[i] * 100 / max;
-        }
-
-        System.out.println(result/N);
+        System.out.println(result * 100 / N / max);
     }
 }
