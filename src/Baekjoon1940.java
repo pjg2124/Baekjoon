bfa0f202782eefda0ba28e8f994af65d83e74067
parkjingam <pjg2124@naver.com>
2023-07-19 16:15:14 +0900

    baekjoon1940

---
+++ src/Baekjoon1940.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Baekjoon1940 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int start = 0, end = N-1, cnt = 0;
+
+        while(arr[start] < arr[end]) {
+            if (arr[start] + arr[end] < M) start++;
+            else if (arr[start] + arr[end] > M) end--;
+            else {
+                end--;
+                start++;
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
