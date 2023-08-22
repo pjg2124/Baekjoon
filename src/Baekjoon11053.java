5c599a95266dde417f80e52c4037089d526f1c3d
parkjingam <pjg2124@naver.com>
2023-08-22 17:13:45 +0900

    baekjoon11053

---
+++ src/Baekjoon11053.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i : dp) {
+            result = Math.max(result, i);
+        }
+        System.out.println(result);
+    }
+}
