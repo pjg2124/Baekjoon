b2b7d8cd90ee5f906aa3ba152ccd8477eb44129c
parkjingam <pjg2124@naver.com>
2023-08-22 20:49:41 +0900

    baekjoon1912

---
+++ src/Baekjoon1912.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = dp[i-1] + arr[i];
+
+            if (dp[i] < arr[i]) dp[i] = arr[i];
+        }
+
+        int result = -1001;
+        for (int i : dp) {
+            result = Math.max(result, i);
+        }
+
+        System.out.println(result);
+    }
+}
