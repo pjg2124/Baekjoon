d8b139af6f69d1a9f6eaa02a1a6b99e30596efcf
Jingam <pjg2124@git.ajou.ac.kr>
2023-12-01 11:39:15 +0900

    baekjoon2775

---
+++ src/Baekjoon2775.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Baekjoon2775 {
+    static int[][] dp = new int[15][15];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 1; i < 15; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j < 15; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            int k = sc.nextInt();
+            int n = sc.nextInt();
+
+            System.out.println(dp[k][n]);
+        }
+    }
+}
